a453d2817af64dd98490039cec245d54f5e8f688
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-07-10 19:48:35 +0530

    desktop - funcionality-added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const grid = document.querySelector('.card')
 const button = document.querySelector('.card__button')
 
 button.addEventListener('click',() => {
-    console.log(grid.childNodes)
+    
     if(document.body.clientWidth < 700) {
         
         const authorImg = document.querySelector('.card__authorimg')
@@ -57,6 +57,49 @@ button.addEventListener('click',() => {
             authorName.style.display = 'block'
         }
     } else {
-        console.log('dekstop')
+        const tooltipText = document.querySelector('#tooltiptext')
+        const toolTip = document.querySelector('.card__button')
+
+        if( !tooltipText ) {
+            
+            let toolTipText = document.createElement('span')
+            toolTipText.setAttribute('id','tooltiptext')
+            toolTipText.style.position = 'absolute'
+            toolTipText.style.visibility = 'visible'
+            toolTipText.style.zIndex = 1
+            toolTipText.style.display = 'flex'
+            toolTipText.style.justifyContent = 'center'
+            toolTipText.style.alignItems = 'center'
+            toolTipText.style.textTransform = 'uppercase'
+            toolTipText.style.color = 'hsl(212, 23%, 69%)'
+            toolTipText.style.background = 'hsl(217, 19%, 35%)'
+            toolTipText.style.marginBottom = '100px'
+            toolTipText.style.width = '200px'
+            toolTipText.style.height = '50px'
+            toolTipText.style.borderRadius = '10px'
+            toolTipText.innerText = 'share'
+            const facebookImg = document.createElement('img')
+            facebookImg.src = './images/icon-facebook.svg'
+            facebookImg.style.marginLeft='5%'
+            toolTipText.appendChild(facebookImg)
+            const twitterImg = document.createElement('img')
+            twitterImg.src = './images/icon-twitter.svg'
+            twitterImg.style.marginLeft='5%'
+            toolTipText.appendChild(twitterImg)
+            const pinterestImg = document.createElement('img')
+            pinterestImg.src = './images/icon-pinterest.svg'
+            pinterestImg.style.marginLeft='5%'
+            toolTipText.appendChild(pinterestImg)
+
+            toolTip.appendChild(toolTipText)
+
+        } else if( tooltipText.style.visibility == 'hidden' ) {
+            
+            tooltipText.style.visibility = 'visible'
+        }
+        else if ( tooltipText.style.visibility == 'visible') {
+            
+            tooltipText.style.visibility = 'hidden'
+        }
     }
 })
